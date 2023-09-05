6e3b481dfb9a898006def0ab02611c8ede809d24
minhdtb <minhdtb.83@gmail.com>
2023-09-05 22:46:33 +0700

    initial commit

--- src/pages/driver/list.vue
+++ src/pages/driver/list.vue
@@ -1,7 +1,56 @@
-<template></template>
+<template>
+  <v-data-table-server
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="driverList"
+    :loading="loading"
+    :items-length="total"
+    hover
+    @update:options="handleLoadItems"
+    @click:row="handleClickRow"
+  >
+    <template #item.index="{ index }">
+      {{ index + 1 }}
+    </template>
+    <template #item.avatar="{ item }">
+      <v-avatar>
+        <v-img :src="item.raw.avatar"></v-img>
+      </v-avatar>
+    </template>
+  </v-data-table-server>
+</template>
 <script setup lang="ts">
+import { useDriverStore } from '~/stores/driver';
+
 definePageMeta({
   layout: 'dashboard',
   middleware: ['auth'],
 });
+
+const driverStore = useDriverStore();
+const { driverList, loading, total } = storeToRefs(driverStore);
+
+const headers = [
+  { title: '#', key: 'index' },
+  { title: 'Ảnh', key: 'avatar' },
+  { title: 'Họ tên', key: 'name' },
+  { title: 'Số điện thoại', key: 'phone' },
+  { title: 'Email', key: 'driver_email' },
+  { title: 'Trạng thái', key: 'authStatus' },
+  { title: 'Sẵn sàng', key: 'driveStatus' },
+  { title: 'Hoạt động', key: 'activeStatus' },
+];
+
+const itemsPerPage = ref(10);
+
+const handleLoadItems = async (options: any) => {
+  await driverStore.list({
+    limit: options.itemsPerPage,
+    page: options.page,
+  });
+};
+
+const handleClickRow = (_: Event, { item }: any) => {
+  navigateTo(`customer/${item.raw.id}`);
+};
 </script>
---
+++ src/stores/driver.ts
@@ -0,0 +1,63 @@
+import { ListOptions } from '~/utils/types';
+
+export interface Driver {
+  id?: string;
+  name?: string;
+  phone?: string;
+  user_email?: string;
+  avatar?: string;
+  authStatus?: string;
+}
+
+export const useDriverStore = defineStore('driverStore', () => {
+  const pb = usePb();
+  const appConfig = useAppConfig();
+
+  const loading = ref(false);
+  const errorMessage = ref<string | undefined>(undefined);
+  const isError = computed(() => errorMessage.value !== undefined);
+  const listOptions = ref<ListOptions | undefined>(undefined);
+  const driverList = ref<Driver[]>([]);
+  const total = ref<number>(0);
+  const current = ref<Driver | undefined>(undefined);
+
+  const actions = {
+    async list(options?: ListOptions) {
+      try {
+        loading.value = true;
+        if (options) {
+          listOptions.value = options;
+        }
+        const res = await pb
+          .collection('driver')
+          .getList(listOptions.value?.page, listOptions.value?.limit);
+        driverList.value = res.items.map((it) => {
+          if (it['avatar']) {
+            it['avatar'] = `${appConfig.backend.url}/api/files/driver/${it.id}/${it['avatar']}`;
+          }
+          return it;
+        });
+        total.value = res.totalItems;
+      } catch (err) {
+        if (typeof err === 'string') {
+          errorMessage.value = err;
+        } else if (err instanceof Error) {
+          errorMessage.value = err.message;
+        }
+      } finally {
+        loading.value = false;
+      }
+    },
+  };
+
+  return {
+    loading,
+    isError,
+    driverList,
+    total,
+    listOptions,
+    current,
+    errorMessage,
+    ...actions,
+  };
+});
