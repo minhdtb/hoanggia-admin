0875bbd157db1a8d1f6e45f65a4b03dbe44cf1ee
minhdtb <minhdtb.83@gmail.com>
2023-09-05 22:32:54 +0700

    initial commit

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,20 +1,25 @@
-import {createVuetify} from 'vuetify';
+import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import {VDataTable, VDataTableServer} from 'vuetify/labs/VDataTable';
+import { VDataTable, VDataTableServer } from 'vuetify/labs/VDataTable';
+import { vi } from 'vuetify/locale';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const vuetify = createVuetify({
-        ssr: false,
-        components: {
-            ...components,
-            VDataTable,
-            VDataTableServer,
-        },
-        directives,
-        theme: {
-            defaultTheme: 'light',
-        },
-    });
-    nuxtApp.vueApp.use(vuetify);
+  const vuetify = createVuetify({
+    ssr: false,
+    components: {
+      ...components,
+      VDataTable,
+      VDataTableServer,
+    },
+    locale: {
+      locale: 'vi',
+      messages: { vi },
+    },
+    directives,
+    theme: {
+      defaultTheme: 'light',
+    },
+  });
+  nuxtApp.vueApp.use(vuetify);
 });
