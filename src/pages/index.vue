fb34f7f05e4744bc59c5154c393f42d76f5b1a54
minhdtb <minhdtb.83@gmail.com>
2023-09-05 17:30:51 +0700

    initial commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <h1>INDEX</h1>
 </template>
 <script setup lang="ts">
 definePageMeta({
   layout: 'dashboard',
   middleware: ['auth'],
 });
+
+navigateTo('/customer/list')
 </script>
