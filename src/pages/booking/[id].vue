7d806640204ad426af9a4cb236297d937f861c5a
minhdtb <minhdtb.83@gmail.com>
2023-09-11 15:55:42 +0700

    update coding

--- src/pages/booking/[id].vue
+++ src/pages/booking/[id].vue
@@ -1,11 +1,1 @@
 <template></template>
-<script setup lang="ts">
-useHead({
-  title: `Hoang Gia Driver - Lái xe`,
-});
-
-definePageMeta({
-  layout: 'dashboard',
-  middleware: ['auth'],
-});
-</script>
--- src/pages/booking/list.vue
+++ src/pages/booking/list.vue
@@ -1,7 +1,81 @@
-<template></template>
+<template>
+  <v-data-table-server
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="bookingList"
+    :loading="loading"
+    :items-length="total"
+    hover
+    @update:options="handleLoadItems"
+    @click:row="handleClickRow"
+  >
+    <template #item.index="{ index }">
+      {{ index + 1 }}
+    </template>
+    <template #item.user="{ item }">
+      <a :href="`/customer/${item.raw.expand?.user?.id}`">{{ item.raw.expand?.user?.name }}</a>
+    </template>
+    <template #item.driver="{ item }">
+      <a :href="`/driver/${item.raw.expand?.driver?.id}`">{{ item.raw.expand?.driver?.name }}</a>
+    </template>
+    <template #item.from="{ item }">
+      {{ item.raw.from.formatted_address }}
+    </template>
+    <template #item.to="{ item }">
+      {{ item.raw.to.formatted_address }}
+    </template>
+    <template #item.pickupDate="{ item }">
+      {{ $moment(item.raw.pickupDate).subtract(7, 'hour').format('DD/MM/YYYY HH:mm') }}
+    </template>
+    <template #item.distance="{ item }">
+      {{ item.raw.distance.text }}
+    </template>
+    <template #item.fee="{ item }">
+      {{ new Intl.NumberFormat('vi-VN').format(item.raw.fee ?? 0) }}
+    </template>
+    <template #item.created="{ item }">
+      {{ $moment(item.raw.created).format('DD/MM/YYYY HH:mm') }}
+    </template>
+  </v-data-table-server>
+</template>
 <script setup lang="ts">
+import { useBookingStore } from '~/stores/booking';
+
+useHead({
+  title: `Hoang Gia Driver - Khách hàng`,
+});
+
 definePageMeta({
   layout: 'dashboard',
   middleware: ['auth'],
 });
+
+const bookingStore = useBookingStore();
+const { bookingList, loading, total } = storeToRefs(bookingStore);
+
+const headers = [
+  { title: '#', key: 'index' },
+  { title: 'Khách hàng', key: 'user' },
+  { title: 'Lái xe', key: 'driver' },
+  { title: 'Điểm đón', key: 'from' },
+  { title: 'Điểm đến', key: 'to' },
+  { title: 'Thời gian đón', key: 'pickupDate' },
+  { title: 'Quãng đường', key: 'distance' },
+  { title: 'Cước phí', key: 'fee' },
+  { title: 'Trạng thái', key: 'status' },
+  { title: 'Ngày tạo', key: 'created' },
+];
+
+const itemsPerPage = ref(10);
+
+const handleLoadItems = async (options: any) => {
+  await bookingStore.list({
+    limit: options.itemsPerPage,
+    page: options.page,
+  });
+};
+
+const handleClickRow = (_: Event, { item }: any) => {
+  navigateTo(`${item.raw.id}`);
+};
 </script>
